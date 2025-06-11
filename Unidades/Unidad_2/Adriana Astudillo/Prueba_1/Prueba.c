3604596beeb38db4ba031620f7a8061388293f25
Alan-Steven-Nero-Arboleda <asnero@espe.edu.ec>
2025-06-11 08:32:14 -0500

    Update Prueba.c

--- Unidades/Unidad_2/Adriana Astudillo/Prueba_1/Prueba.c
+++ Unidades/Unidad_2/Adriana Astudillo/Prueba_1/Prueba.c
@@ -1,1 +1,32 @@
+# include <stdio.h>
+#define max
+int main () {
+int i,j;
+int f,c;
+int n;
+int max;
+int mat[f][c];
+max=0
+//numero de filas y columnas
+printf("Ingrese el numero de filas: ");
+    scanf("%d", &f);
+pritf("Ingrese el numero de columnas",);
+    scanf("%d", &c);
+//Matriz generada
+    for (i = 0; i < f; i++) {
+        for (j = 0; j < c; j++) {
+            matriz[i][j] = 0;
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+//Ingrsar fila
+printf("Ingrese la fila i: ",f-1);
+    scanf("%d", &f_i);
+    if (i<=0, i<f)
+        max=
 
+    }
+
+}
+    }
