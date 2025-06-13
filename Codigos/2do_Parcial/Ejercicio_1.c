504417334bd294733931a6c8d4e524e487c742ef
SarahiFernandaMunozMoreno <sfmunoz2@espe.edu.ec>
2025-06-13 07:19:49 -0500

    Update Ejercicio_1.c

--- Codigos/2do_Parcial/Ejercicio_1.c
+++ Codigos/2do_Parcial/Ejercicio_1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #define MAX 100
-//El sistema debe solicitar al usuario el número de términos (n) a generar, con un máximo de MAX = 100.
-//El sistema debe validar que n esté en el rango permitido (1 a 100). Si no, mostrará un mensaje de error y terminará.
-//El sistema debe calcular cada término de la sucesión usando la fórmula y almacenarlo en un vector.
-//El sistema debe imprimir los términos generados en formato lista.
+//R_1:El sistema debe solicitar al usuario el número de términos (n) a generar, con un máximo de MAX = 100.
+//R_2:El sistema debe validar que n esté en el rango permitido (1 a 100). Si no, mostrará un mensaje de error y terminará.
+//R_3:El sistema debe calcular cada término de la sucesión usando la fórmula y almacenarlo en un vector.
+//R_4:El sistema debe imprimir los términos generados en formato lista.
 int main() {
     int n;
     int vec[MAX];
