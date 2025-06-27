58488f9a1aa22430bf2259edef84dde3851abf41
Alan-Steven-Nero-Arboleda <asnero@espe.edu.ec>
2025-06-27 08:23:41 -0500

    Create juego_1.c
    Signed-off-by: Alan-Steven-Nero-Arboleda <asnero@espe.edu.ec>

---
+++ Notas/juego_1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //Lbreria de Gestion de tiempos
+
+#define MAX_INTENTOS 5
+
+int main() {
+    int num[MAX_INTENTOS];
+    int num_sec, intento;
+    int i;
+
+    // Número aleatorio entre 1 y 100
+    srand(time(NULL));
+    num_sec = rand() % 100 + 1;
+
+    // Solicitar intentos del ciclo for
+    for (i = 0; i < MAX_INTENTOS; i++) {
+        printf("Intento %d: Ingresa un numero: ", i + 1);
+        scanf("%d", &intento);
+        num[i] = intento;
+
+    // Mensaje secreto al adivinar
+        if (intento == num_sec) {
+            printf("Felicidades Adivinaste el numero secreto.\n");
+            printf("Eres un gran jugador.\n");
+            break;
+    // Validacion de Intentos
+        } else if (intento < num_sec) {
+            printf("Demasiado bajo.\n");
+        } else {
+            printf("Demasiado alto.\n");
+        }
+    }
+    // Mostrar los intentos realizados
+    printf("\nTus intentos fueron: ");
+    for (int j = 0; j <= i; j++) {
+        printf("%d ", num[j]);
+    }
+    printf("\n");
+    return 0;
+}
